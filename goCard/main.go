9415082a8ad2d1057e865f4583fe9a263ccdcf90
Mongkol KORTAWEESAK <mongkol.kortaweesak@amadeus.com>
2018-11-01 16:14:33 +0700

    f

--- goCard/main.go
+++ goCard/main.go
@@ -1,16 +1,9 @@
 package main
 
-import "fmt"
 
 func main() {
 
-	cards := deck{"Ace of Diamonds", card()}
-	cards = append(cards, "Six of Spades")
-	fmt.Println(cards)
-	
-	cards.print()
-}
+	cards := newDeck()
 
-func card() string {
-	return "Five of Diamonds"
+	cards.print()
 }
