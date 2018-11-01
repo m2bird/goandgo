5f5bd91f1e1d49be423ab72a2a92b9ef610aac83
Mongkol KORTAWEESAK <mongkol.kortaweesak@amadeus.com>
2018-11-01 15:46:33 +0700

    f

--- goCard/main.go
+++ goCard/main.go
@@ -4,13 +4,11 @@ import "fmt"
 
 func main() {
 
-	cards := []string{"Ace of Diamonds", card()}
+	cards := deck{"Ace of Diamonds", card()}
 	cards = append(cards, "Six of Spades")
 	fmt.Println(cards)
 	
-	for i, card := range cards{
-		fmt.Println(i,card)
-	}
+	cards.print()
 }
 
 func card() string {
